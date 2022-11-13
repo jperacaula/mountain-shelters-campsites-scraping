7b23f50bbcba84d349e1e61bc19f008e1e236adc
sarajose <sarajose6@gmail.com>
2022-11-13 19:42:27 +0100

    Fixed get routes and added get image functionality

--- src/dynamic_scrapper.py
+++ src/dynamic_scrapper.py
@@ -2,52 +2,58 @@
 """
 Created on Sun Nov 13 00:08:11 2022
 @author: Sara Jose,  Joan Peracaula
-Description: Dynamic web scrapper that longest and shortest route from a shelter
+Description: Dynamic web scrapper that saves images of the maps for each shelter and gets its closests routes
 
 ChromeDriver 107.0.5304.62
 Chrome version: Version 107.0.5304.107 (Official Build) (64-bit)
 """
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import time
 
-def route_scrapper(url):    
+def route_scrapper(url):
    
     #Choose chrome driver from selenium   
     PATH = "C:\Program Files (x86)\chromedriver.exe"
     driver = webdriver.Chrome(PATH)
-    #(Hard coded, pass url from main function)
-    driver_main = driver.get(url)
+    #driver_main = driver.get(url)    
     
+    # Get map images of each one of the shelters
+    get_images(driver, url)
     
-    #route2 = driver.find_element(By.LINK_TEXT, "GR-241") 
-    #links= driver.find_elements(By.CLASS_NAME, "btn-link")
-
-    GR_links = driver.find_elements(By.PARTIAL_LINK_TEXT, 'GR')
+    #Get nearby routes
     try:
-        GR_name = GR_links[0].text
-        GR_links[0].click()
-        GR_distance = driver.find_element(By.XPATH, '/html/body/div[2]/div[3]/div/div[3]/div/div[2]/div[1]/div[1]/div[1]/span').text
-        driver_main = driver.get(url)
-    except IndexError:
-        GR_name = '?'
-        GR_distance = '?'
-
-    PR_links = driver.find_elements(By.PARTIAL_LINK_TEXT, 'PR')
-    
-    try:
-        PR_name = PR_links[0].text
-        PR_links[0].click()
-        PR_distance = driver.find_element(By.XPATH, '/html/body/div[2]/div[3]/div/div[3]/div/div[2]/div[1]/div[1]/div[1]/span').text
-    except IndexError:
-        PR_name = '?'
-        PR_distance = '?'
-    
+        routes_section = driver.find_element(By.CLASS_NAME, "nearby-routes")
+        routes = routes_section.find_elements(By.TAG_NAME, "a")
+        routes_names = []
         
+        for route in routes:
+            routes_names.append(route.text)    
+        
+        # Gets the distance of the first route, (Functional but discared due to long loading times)
+        #routes[0].click()
+        #distance = driver.find_element(By.XPATH, '/html/body/div[2]/div[3]/div/div[3]/div/div[2]/div[1]/div[1]/div[1]/span').text
+        #driver_main = driver.get(url)
+        
+    except IndexError:
+        routes_names = '?'
     
-    GR = GR_name , GR_distance
-    PR = PR_name , PR_distance
-    
-    return GR, PR
+    driver.quit()
+    print(routes_names)
 
-#route2 = driver.find_element(By.LINK_TEXT, "GR-241") 
-#route2.click()
+    return routes_names
+
+
+def get_images(driver, url):
+    # Map zoom in    
+    zoom = driver.find_element(By.CLASS_NAME, "maplibregl-ctrl-zoom-in")
+    for var in list(range(2)):
+        zoom.click()
+        time.sleep(0.1)
+    
+    # Map screenshot
+    time.sleep(1.2)
+    mapa = driver.find_element(By.CLASS_NAME, "maplibregl-canvas")
+    
+    png_path = "images/" + url.split("/")[-1] + ".png"
+    mapa.screenshot(png_path)    
