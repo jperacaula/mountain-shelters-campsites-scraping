737f7cadee6a5a608000b84a79d6e3ecd3cec28b
Joan <jperacaula1@gmail.com>
2022-11-14 19:14:52 +0100

    add description and more contact information to shelters

--- src/scrapper.py
+++ src/scrapper.py
@@ -1,6 +1,9 @@
 import requests
 import random
 from bs4 import BeautifulSoup
+import pandas as pd
+from tabulate import tabulate
+
 
 headers = {
     "Accept": "text/html",
@@ -21,8 +24,9 @@ all_list_items = list_page.find_all('a')
 all_links = ["https://www.walkaholic.me" + item.get('href') for item in all_list_items]
 
 # Shuffle links to access randomly (more human-like, less prone to be detected by the system)
-random.shuffle(all_links)
-
+#random.shuffle(all_links)
+    
+shelters_list = []
 for link in all_links[:10]: 
     page = requests.get(link, headers=headers)  # Change Referer header to the previous link (?)
     soup = BeautifulSoup(page.text, 'html.parser')
@@ -35,7 +39,7 @@ for link in all_links[:10]:
     head = soup.find(class_='name').contents[0].strip()
     place_type, name = head.split(" - ", maxsplit=1)
 
-    # Extract capacity, fee and altitude  
+    # Extract capacity, fee and altitude if available
     div_basic_details = soup.find(class_='basic-details')
     capacity, fee, altitude = '?', '?', '?'
     if div_basic_details.find(class_='capacity'):  # Apparently, capacity shouldn't be null
@@ -47,8 +51,48 @@ for link in all_links[:10]:
     if div_basic_details.find(class_='altitude'):  # Altitude could be null
         altitude = div_basic_details.find(class_='altitude').find_next('span').contents[0]
 
+    # Extract description, if available
+    description = '?'
+    if soup.find(class_='description'):
+        description = soup.find(class_='description').contents[0]
+
+    # Extract telephone, website and email if available
+    telephone, website, email = '?', '?', '?'
+    div_contact = soup.find(class_='mt-2')
+    div_contact.find(class_='row').decompose()  # Exclude first row with title (required for better extracting next contents that don't have unique CSS classes)
+
+    if div_contact.find(class_='link telephone'):
+        telephone = div_contact.find(class_='link telephone')['href']
+        div_contact.find(class_='link telephone').find_parent().decompose()  # Exclude this element (required for better extracting next contents that don't have unique CSS classes)
+
+    if div_contact.find(class_='link website'):
+        website = div_contact.find(class_='link website')['href']   
+        div_contact.find(class_='link website').find_parent().decompose()  # Exclude this element (required for better extracting next contents that don't have unique CSS classes)
+
+    if div_contact.find(class_='link email'):
+        email = div_contact.find(class_='link email')['href']
+        div_contact.find(class_='link email').find_parent().decompose()  # Exclude this element (required for better extracting next contents that don't have unique CSS classes)
+
+    # Extract Hiking association and Guard name(s), if available
+    hiking_association, guard_names = '?', '?'
+    extra_rows = div_contact.find_all(class_='row')
+    for row in extra_rows:
+        label = row.find('b').contents[0]
+        if label == 'Hiking association:':
+            hiking_association = label.next.strip()
+        elif label == 'Guard name(s):':
+            guard_names = label.next.strip()
+
+    # Debug print
     print(link)
-    print(places_list)
-    print(place_type + ", " + name)
-    print(capacity + ", " + fee + ", " + altitude)
     print()
+
+    shelters_list.append({'Place type': place_type, 'Name': name, 'Place list': places_list,
+                    'Capacity': capacity, 'Fee': fee, 'Altitude': altitude, 'Telephone': telephone, 
+                    'Website': website, 'Email': email, 'Hiking association': hiking_association,
+                    'Guard name(s)': guard_names,
+                    'Description': description})
+
+
+df = pd.DataFrame.from_dict(shelters_list)
+df.to_csv("refugisdemuntanya.csv")
