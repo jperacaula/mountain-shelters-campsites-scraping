a545a4e930e000389dec5fb4d5163eba3d2aa160
sarajose <sarajose6@gmail.com>
2022-11-16 23:55:58 +0100

    Functions save, get urls and get attributes defined

--- source/scrapper.py
+++ source/scrapper.py
@@ -1,3 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 16, 2022
+@author: Sara Jose, Joan Peracaula
+"""
+
 import os
 import sys
 import requests
@@ -5,144 +11,70 @@ import random
 from bs4 import BeautifulSoup
 import pandas as pd
 from tabulate import tabulate
+import attributes_scrapper as scrap
 
-headers = {
-    "Accept": "text/html",
-    "Cache-Control": "no-cache",
-    'dnt': '1',
-    'upgrade-insecure-requests': '1',   
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36",
-    "Referer": "https://www.walkaholic.me"
-}
-
-# Scrap shelters and campsites landings to obtain a full list of links
-all_links = []
-for landing in ['shelter', 'campsite']:
-    page = requests.get('https://www.walkaholic.me/'+landing, headers=headers)
-    soup = BeautifulSoup(page.text, 'html.parser')
-
-    list_links = soup.find(class_='list-page').find_all('a')
-    all_links += ["https://www.walkaholic.me" + a.get('href') for a in list_links]
-
-# Shuffle links to access randomly (more human-like, less prone to be detected by the system)
-random.shuffle(all_links)
+def get_urls():
+    headers = {
+        "Accept": "text/html",
+        "Cache-Control": "no-cache",
+        'dnt': '1',
+        'upgrade-insecure-requests': '1',   
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36",
+        "Referer": "https://www.walkaholic.me"
+    }
     
-accommodations_list = []
-for link in all_links[:10]: 
-    page = requests.get(link, headers=headers)
-    soup = BeautifulSoup(page.text, 'html.parser')
-
-    # Extract country and subregions
-    breadcrump = soup.find_all(class_='breadcrumb-item')
-    places_list = [x.find('a').contents[0].strip() for x in breadcrump]  # Country, region, subregion (the last could be null)
-
-    # Extract place type and name
-    head = soup.find(class_='name').contents[0].strip()
-    place_type, name = head.split(" - ", maxsplit=1)
-
-    # Extract capacity, fee and altitude if available
-    div_basic_details = soup.find(class_='basic-details')
-    capacity, fee, altitude = '?', '?', '?'
-    if div_basic_details.find(class_='capacity'):  # Apparently, capacity shouldn't be null
-        capacity = div_basic_details.find(class_='capacity').find_next('span').contents[0]
-
-    if div_basic_details.find(class_='fee'):  # Apparently, fee shouldn't be null
-        fee = div_basic_details.find(class_='fee').find_next('span').contents[0]
+    # Scrap shelters and campsites landings to obtain a full list of links
+    all_links = []
+    for landing in ['shelter', 'campsite']:
+        page = requests.get('https://www.walkaholic.me/'+landing, headers=headers)
+        soup = BeautifulSoup(page.text, 'html.parser')
     
-    if div_basic_details.find(class_='altitude'):  # Altitude could be null
-        altitude = div_basic_details.find(class_='altitude').find_next('span').contents[0]
-
-    # Extract description, if available
-    description = '?'
-    if soup.find(class_='description'):
-        description = soup.find(class_='description').contents[0]
-
-    # Extract Contact information if available
-    telephone, website, email, hiking_association, guard_names = '?', '?', '?', '?', '?'
-    div_contact = soup.find(class_='contact')
-    if div_contact:
-        div_contact.find(class_='row').decompose()  # Exclude first row with title (required for better extracting next contents that don't have unique CSS classes)
-
-        if div_contact.find(class_='link telephone'):
-            telephone = div_contact.find(class_='link telephone')['href']
-            div_contact.find(class_='link telephone').find_parent().decompose()  # Exclude this element (required for better extracting next contents that don't have unique CSS classes)
-
-        if div_contact.find(class_='link website'):
-            website = div_contact.find(class_='link website')['href']   
-            div_contact.find(class_='link website').find_parent().decompose()  # Exclude this element (required for better extracting next contents that don't have unique CSS classes)
-
-        if div_contact.find(class_='link email'):
-            email = div_contact.find(class_='link email')['href']
-            div_contact.find(class_='link email').find_parent().decompose()  # Exclude this element (required for better extracting next contents that don't have unique CSS classes)
-
-        # Extract Hiking association and Guard name(s), if available
-        hiking_association, guard_names = '?', '?'
-        extra_rows = div_contact.find_all(class_='row')
-        for row in extra_rows:
-            label = row.find('b').contents[0]
-            if label == 'Hiking association:':
-                hiking_association = label.next.strip()
-            elif label == 'Guard name(s):':
-                guard_names = label.next.strip()
-
-    # Extract list of services, if available
-    services_list = []
-    if soup.find(class_='service'):
-        services = soup.find_all(class_='service')
-        for service in services:
-            services_list.append(service['title'])    
-             
-    # Extract Location/'How to get there' information
-    lat_long = '?'
-    access, zones, emplacement = [], [], []
-    div_access = soup.find(class_='how-to-get-there')
-    if div_access:
-        div_access.find(class_='row').decompose()  # Decompose title row
+        list_links = soup.find(class_='list-page').find_all('a')
+        all_links += ["https://www.walkaholic.me" + a.get('href') for a in list_links]
+    
+    # Shuffle links to access randomly (more human-like, less prone to be detected by the system)
+    random.shuffle(all_links)
         
-        # Get latitude and longitude
-        if div_access.find(class_='row'):
-            div_access.find(class_='col-12').decompose()
-            lat_long = div_access.find(class_='row coordinates').find_next('span').contents[0]
-            div_access.find(class_='row coordinates').decompose()
+    return headers, all_links
+
+def get_atributes(headers, all_links):
+    accommodations_list = []
+    
+    for link in all_links[:10]: 
+        page = requests.get(link, headers=headers)
+        soup = BeautifulSoup(page.text, 'html.parser')
         
-        # Get acces, zones and emplacement
-        if div_access.find(class_='row'):
-            for row in div_access.find_all(class_='row'):
-                text = row.text.replace("\n", "")
-                
-                if text.startswith('Zone'):
-                    zones.append(text)
-        
-                elif text.startswith('Emplacement'):
-                    emplacement.append(text)
-                    
-                elif text.startswith('Access'):
-                    access.append(text) 
+        # Get all attributes
+        places_list = scrap.get_country_subregions(soup)
+        place_type, name = scrap.get_placetype(soup)
+        capacity, fee, altitude = scrap.get_capacity_fee_altitude(soup)
+        description= scrap.get_description(soup)
+        telephone, website, email, hiking_association, guard_names = scrap.get_contact(soup)
+        services_list = scrap.get_services(soup)
+        lat_long, access, zones, emplacement = scrap.get_location(soup)
+        routes_list = scrap.get_routes(soup)
+                       
+        # Append new scrapped accomodation to the list
+        accommodations_list.append({
+                'Place type': place_type, 'Name': name, 'Place list': places_list,
+                'Capacity': capacity, 'Fee': fee, 'Altitude': altitude, 'Description': description, 
+                'Telephone': telephone, 'Website': website, 'Email': email, 
+                'Hiking association': hiking_association, 'Guard name(s)': guard_names, 
+                'Services': services_list, 'Coordinates': lat_long, 'Acces': access, 'Zones': zones,
+                'Emplacement': emplacement, 'Nearby routes': routes_list
+            })        
+    
+    print("Scrapping complete!")
+    return accommodations_list
 
-    # Extract Nearby hiking routes names
-    routes_list = []
-    routes = soup.find("div", class_='nearby-routes')
-    if routes:
-        for a in routes.find_all('a', href=True):
-            routes_list.append(a.text.strip())
-                   
-    # Append new scrapped accomodation to the list
-    accommodations_list.append({
-            'Place type': place_type, 'Name': name, 'Place list': places_list,
-            'Capacity': capacity, 'Fee': fee, 'Altitude': altitude, 'Description': description, 
-            'Telephone': telephone, 'Website': website, 'Email': email, 
-            'Hiking association': hiking_association, 'Guard name(s)': guard_names, 
-            'Services': services_list, 'Coordinates': lat_long, 'Acces': access, 'Zones': zones,
-            'Emplacement': emplacement, 'Nearby routes': routes_list
-        })
-
-print("Scrapping complete!")
-
-script_path = os.path.dirname(os.path.abspath(sys.argv[0]))
-root_path = os.path.dirname(script_path)
-dataset_path = os.path.join(root_path, "dataset/shelters_and_campsites.csv")
-print("Saving dataset to: " + dataset_path) 
-
-# Create pandas dataframe with the whole scrapped data and save it as CSV in the datasets directory 
-df = pd.DataFrame.from_dict(accommodations_list)
-df.to_csv(dataset_path)
+def save_dataset(accommodations_list):
+    
+    #Create path for the dataset
+    script_path = os.path.dirname(os.path.abspath(sys.argv[0]))
+    root_path = os.path.dirname(script_path)
+    dataset_path = os.path.join(root_path, "dataset/shelters_and_campsites.csv")
+    print("Saving dataset to: " + dataset_path) 
+    
+    # Create pandas dataframe with the whole scrapped data and save it as CSV in the datasets directory 
+    df = pd.DataFrame.from_dict(accommodations_list)
+    df.to_csv(dataset_path)
